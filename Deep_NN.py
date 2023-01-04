30eb7484f24f165315cb91ce39a9526d6a93b61d
Tristan Bandat <tristan.bandat@gmail.com>
2023-01-04 17:01:36 +0100

    Deep_NN: Update imports and resize model

--- Deep_NN.py
+++ Deep_NN.py
@@ -1,16 +1,14 @@
-import pandas as pd
 import tensorflow_datasets as tfds
-import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-import matplotlib.pyplot as plt
+# from tensorflow.keras.utils import plot_model
 
 
 def create_model():
     model = keras.Sequential()
     model.add(layers.Dense(4096, input_shape=(4096,), activation='relu'))
-    for i in range(10):
-        model.add(layers.Dense(10000, activation='relu'))
+    for i in range(20):
+        model.add(layers.Dense(2000, activation='relu'))
     model.add(layers.Dense(685, activation='sigmoid'))
 
     return model
@@ -27,8 +25,10 @@ def main():
     #                      as_supervised=True, split='valid')
     model = create_model()
     model.compile("adam", "mean_squared_error", metrics=["accuracy"])
-    keras.utils.plot_model(model, show_shapes=True, rankdir="LR")
+    # plot_model(model, show_shapes=True, rankdir="LR")
     model.fit(train_ds, epochs=10)
+    _, score = model.evaluate(test_ds)
+    print(f'Deep_NN (1+10+1): {score}')
     pass
 
 
