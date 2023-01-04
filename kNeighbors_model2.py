4b3d2dde4e88824727f0860432e5028233a4e139
Tristan Bandat <tristan.bandat@gmail.com>
2023-01-04 17:02:40 +0100

    kNeighbors: Script to find best number of neighbors

---
+++ kNeighbors_model2.py
@@ -0,0 +1,41 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+import tensorflow_datasets as tfds
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def main():
+    train_ds = tfds.load('music4_all_onion_dc:1.0.1', data_dir='data/', batch_size=1,
+                         as_supervised=True, split='train')
+    test_ds = tfds.load('music4_all_onion_dc:1.0.1', data_dir='data/', batch_size=1,
+                        as_supervised=True, split='test')
+    # valid_ds = tfds.load('music4_all_onion_dc:1.0.1', data_dir='data/', batch_size=1,
+    #                      as_supervised=True, split='valid')
+
+    xy_train = list(train_ds.as_numpy_iterator())
+    x_train = [x[0][0] for x in xy_train]
+    y_train = [x[1][0] for x in xy_train]
+    xy_test = list(test_ds.as_numpy_iterator())
+    x_test = [x[0][0] for x in xy_test]
+    y_test = [x[1][0] for x in xy_test]
+
+    num_neighbors_list = range(2, 10)
+    train_scores = list()
+    test_scores = list()
+    valid_score = 0
+    model_list = list()
+
+    for num_neighbors in num_neighbors_list:
+        model = KNeighborsClassifier(n_neighbors=num_neighbors)
+        model.fit(x_train, y_train)
+        train_scores.append(model.score(x_train[:2000], y_train[:2000]))
+        print(f'Train score ({num_neighbors}): {train_scores[-1]}')
+        test_scores.append(model.score(x_test, y_test))
+        print(f'Test score ({num_neighbors}): {test_scores[-1]}')
+        model_list.append(model)
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
