df662761da9af00fcc0a1c5ac817fa264e79e0d4
Tristan Bandat <tristan.bandat@gmail.com>
2023-01-04 17:10:30 +0100

    Update kN-classifier to display validation scores

--- kNeighbors_model2.py
+++ kNeighbors_model2.py
@@ -9,8 +9,8 @@ def main():
                          as_supervised=True, split='train')
     test_ds = tfds.load('music4_all_onion_dc:1.0.1', data_dir='data/', batch_size=1,
                         as_supervised=True, split='test')
-    # valid_ds = tfds.load('music4_all_onion_dc:1.0.1', data_dir='data/', batch_size=1,
-    #                      as_supervised=True, split='valid')
+    valid_ds = tfds.load('music4_all_onion_dc:1.0.1', data_dir='data/', batch_size=1,
+                         as_supervised=True, split='valid')
 
     xy_train = list(train_ds.as_numpy_iterator())
     x_train = [x[0][0] for x in xy_train]
@@ -18,11 +18,13 @@ def main():
     xy_test = list(test_ds.as_numpy_iterator())
     x_test = [x[0][0] for x in xy_test]
     y_test = [x[1][0] for x in xy_test]
+    xy_valid = list(valid_ds.as_numpy_iterator())
+    x_valid = [x[0][0] for x in xy_valid]
+    y_valid = [x[1][0] for x in xy_valid]
 
     num_neighbors_list = range(2, 10)
     train_scores = list()
     test_scores = list()
-    valid_score = 0
     model_list = list()
 
     for num_neighbors in num_neighbors_list:
@@ -33,7 +35,8 @@ def main():
         test_scores.append(model.score(x_test, y_test))
         print(f'Test score ({num_neighbors}): {test_scores[-1]}')
         model_list.append(model)
-
+    for i, model in enumerate(model_list):
+        print(f'Model {i+1}: {model.score(x_valid, y_valid)}')
     pass
 
 
