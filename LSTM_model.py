4482c5c2d87d2f9de412d543e92f67eb7ccb76d5
Tristan Bandat <tristan.bandat@gmail.com>
2023-01-04 11:31:27 +0100

    Implement simple LSTM model
    Accuracy ~7%

--- LSTM_model.py
+++ LSTM_model.py
@@ -1,37 +1,45 @@
 import pandas as pd
 import tensorflow_datasets as tfds
 import tensorflow as tf
-from tensorflow import keras
+import tensorflow.keras
 from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 
 
-def main():
-    # tf.debugging.set_log_device_placement(True)
+def create_model():
+    # define a sequential model
+    model = tf.keras.models.Sequential([
+        layers.Embedding(input_dim=4096, output_dim=1024),
+        # layers.LSTM(128),
+        layers.LSTM(128, return_sequences=True),
+        # layers.LSTM(128),
+        layers.LSTM(64),
+        # layers.Dense(1),
+        layers.Dense(1000, activation='relu'),
+        layers.Dense(1000, activation='relu'),
+        layers.Dense(685, activation='sigmoid'),
 
+    ])
+
+    return model
+
+
+def main():
     train_ds = tfds.load('music4_all_onion_dc:1.0.1', data_dir='data/', batch_size=64,
                          as_supervised=True, split='train')
-    test_ds = tfds.load('music4_all_onion_dc:1.0.1', data_dir='data/', batch_size=64,
-                        as_supervised=True, split='test')
-    valid_ds = tfds.load('music4_all_onion_dc:1.0.1', data_dir='data/', batch_size=64,
-                         as_supervised=True, split='valid')
-    # plt.figure(figsize=(10, 10))
-    # for element in ds:
-    #     for i in range(9):
-    #         ax = plt.subplot(3, 3, i + 1)
-    #         plt.plot(element['input'][i].numpy())
-    #         plt.title(f'Input: {i}')
-    #         plt.axis("off")
-    #     break
-    # plt.show()
-
-    input_layer = keras.Input(shape=(4096,), name='input')
-    hidden_layer = layers.Dense(32000, activation='relu')(input_layer)
-    output_layer = layers.Dense(685, activation='sigmoid')(hidden_layer)
-    model = keras.Model(input_layer, output_layer)
+    # test_ds = tfds.load('music4_all_onion_dc:1.0.1', data_dir='data/', batch_size=64,
+    #                     as_supervised=True, split='test')
+    # valid_ds = tfds.load('music4_all_onion_dc:1.0.1', data_dir='data/', batch_size=64,
+    #                      as_supervised=True, split='valid')
+    # input_layer = keras.Input(shape=(4096,), name='input')
+    # hidden_layer = layers.Dense(32000, activation='relu')(input_layer)
+    # output_layer = layers.Dense(685, activation='sigmoid')(hidden_layer)
+    # model = keras.Model(input_layer, output_layer)
+    model = create_model()
     model.compile("adam", "mean_squared_error", metrics=["accuracy"])
     # keras.utils.plot_model(model, show_shapes=True, rankdir="LR")
     model.fit(train_ds, epochs=2)
+    # around 7% accuracy
     pass
 
 
