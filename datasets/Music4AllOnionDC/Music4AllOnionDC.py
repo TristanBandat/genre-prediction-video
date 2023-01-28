ffa9a044ef4099eea334329fa0812b44c662af07
Tristan Bandat <tristan.bandat@gmail.com>
2023-01-28 11:48:20 +0100

    Dataset Version 3.1.0

--- datasets/Music4AllOnionDC/Music4AllOnionDC.py
+++ datasets/Music4AllOnionDC/Music4AllOnionDC.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 
 _DESCRIPTION = """
-Music4AllOnion DC layer dataset with ResNet vectors.
+Music4AllOnion DC layer dataset with VGG19 vectors.
 This dataset has 53 genres based on highest frequency.
 Train/Test/Validation = 80% / 10% / 10%
 Shape scaled to (4096,).
@@ -17,9 +17,9 @@ class Music4AllOnionDC(tfds.core.GeneratorBasedBuilder):
     """
         DatasetBuilder for Music4AllOnionDC dataset.
     """
-    VERSION = tfds.core.Version('2.1.0')
+    VERSION = tfds.core.Version('3.1.0')
     RELEASE_NOTES = {
-        '2.1.0': 'Dataset with ResNet vectors and 53 genres.',
+        '3.1.0': 'Dataset with ResNet vectors and 53 genres.',
     }
 
     def _info(self) -> tfds.core.DatasetInfo:
@@ -43,7 +43,7 @@ class Music4AllOnionDC(tfds.core.GeneratorBasedBuilder):
     def _split_generators(self, dl_manager: tfds.download.DownloadManager):
         """Returns SplitGenerators."""
         path = '../../data/'
-        paths = [path + 'id_resnet.tsv', path + 'id_genres_binary_short.tsv']
+        paths = [path + 'id_vgg19.tsv', path + 'id_genres_binary_short.tsv']
         splits = [0.8, 0.1, 0.1]
         input_df = pd.read_csv(paths[0], sep='\t')
         labels_df = pd.read_csv(paths[1], sep='\t')
@@ -67,17 +67,16 @@ class Music4AllOnionDC(tfds.core.GeneratorBasedBuilder):
                 # print(f'Data with index {i} not found.')
                 continue
 
-            # # compress the vgg19 data
-            # compressed_data = list()
-            # for j in range(0, len(vgg19_data), 2):
-            #     if j < (len(vgg19_data)/2):
-            #         _max = np.max(vgg19_data[j:j + 2])
-            #         compressed_data.append(_max)
-            #     else:
-            #         mean = np.mean(vgg19_data[j:j + 2])
-            #         compressed_data.append(mean)
-            # vgg19_data = np.asarray(compressed_data).reshape((64, 64))
-            # img_data = np.repeat(np.expand_dims(vgg19_data, axis=2), 3, axis=2)
+            # compress the vgg19 data
+            compressed_data = list()
+            for j in range(0, len(data), 2):
+                if j < (len(data)/2):
+                    _max = np.max(data[j:j + 2])
+                    compressed_data.append(_max)
+                else:
+                    mean = np.mean(data[j:j + 2])
+                    compressed_data.append(mean)
+            data = np.asarray(compressed_data, dtype=np.float32)
 
             yield i, {
                 'input': data,
