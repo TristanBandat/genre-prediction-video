b308ff22e4320006df18bc000598e99504473c79
Tristan Bandat <tristan.bandat@gmail.com>
2023-01-29 15:34:12 +0100

    Deep NN with dataset 3.1.0

--- Deep_NN.py
+++ Deep_NN.py
@@ -19,18 +19,18 @@ def create_model():
 
 def main():
     # tf.debugging.set_log_device_placement(True)
-    train_ds = tfds.load('music4_all_onion_dc:2.1.0', data_dir='data/', batch_size=64,
+    train_ds = tfds.load('music4_all_onion_dc:3.1.0', data_dir='data/', batch_size=64,
                          as_supervised=True, split='train')
-    test_ds = tfds.load('music4_all_onion_dc:2.1.0', data_dir='data/', batch_size=64,
+    test_ds = tfds.load('music4_all_onion_dc:3.1.0', data_dir='data/', batch_size=64,
                         as_supervised=True, split='test')
-    valid_ds = tfds.load('music4_all_onion_dc:2.1.0', data_dir='data/', batch_size=64,
+    valid_ds = tfds.load('music4_all_onion_dc:3.1.0', data_dir='data/', batch_size=64,
                          as_supervised=True, split='valid')
 
     model = create_model()
     model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.005), loss=keras.losses.BinaryCrossentropy(),
                   metrics=["accuracy"])
     # keras.utils.plot_model(model, show_shapes=True, rankdir="LR")
-    log_dir = os.path.join('logs', 'Deep_NN', 'ResNet', 'fit', datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+    log_dir = os.path.join('logs', 'Deep_NN', 'VGG19', 'fit', datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
     model.fit(train_ds, epochs=10, validation_data=valid_ds, callbacks=[tensorboard_callback])
     _, score = model.evaluate(test_ds, callbacks=[tensorboard_callback])
